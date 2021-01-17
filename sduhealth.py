72cccfdb26a821319615efaa4364fcaf33d9ad67
viewv <zxnnet@gmail.com>
2021-01-17 23:28:57 +0800

    add lt and execution

--- sduhealth.py
+++ sduhealth.py
@@ -2,21 +2,33 @@ import requests
 import os
 import execjs
 
-from Crypto.Cipher import DES3
+from bs4 import BeautifulSoup
 
 
 def js_from_file(filename):
     # Read Javascript file
+    # so you need nodejs installed on your computer
     with open(filename, 'r', encoding='UTF-8') as f:
         result = f.read()
     return result
 
 
-if __name__ == "__main__":
+def generate_their_RSA(username, password, lt):
+    # return thay call 'RSA' string, note their just return RSA string!
     context = execjs.compile(js_from_file('./des.js'))
-    m = context.call("strEnc",
-                     "201700800398!123456789LT-176706-nKUGsOyV0OIYa0KQgtoa3qFcZVZkeP-cas",
-                     "1", "2", "3")
-    print(m)
-    l = context.call("strDec", m, "1", "2", "3")
-    print(l)
+    rsa = context.call("strEnc", username+password + lt, "1", "2", "3")
+    return rsa
+
+
+def get_lt_And_execution(result):
+    soup = BeautifulSoup(result.content, "html.parser")
+    lt = soup.find(id="lt").get("value")
+    execution = soup.find('input', {'name': 'execution'}).get("value")
+    return {'lt': lt, 'execution': execution}
+
+
+if __name__ == "__main__":
+    url = "https://pass.sdu.edu.cn/cas/login"
+    r = requests.get(url)
+    result = get_lt_And_execution(r)
+    print(result)
