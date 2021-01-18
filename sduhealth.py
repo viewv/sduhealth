7e1bad0f78883f7f5f507ebbac2eb63d11e2823b
viewv <zxnnet@gmail.com>
2021-01-18 12:03:59 +0800

    change into class style

--- sduhealth.py
+++ sduhealth.py
@@ -27,56 +27,61 @@ def get_lt_And_execution(result):
     return {'lt': lt, 'execution': execution}
 
 
-def login(username, password):
-    login_url = "https://pass.sdu.edu.cn/cas/login"
+class SduHealth(object):
+    def __init__(self, username, password) -> None:
+        super().__init__()
+        self.username = username
+        self.password = password
+        self.login_url = "https://pass.sdu.edu.cn/cas/login"
+        self.service_url = "https://service.sdu.edu.cn/tp_up"
+        self.health_url = "https://scenter.sdu.edu.cn/tp_fp/view?m=fp#from=hall&serveID=87dc6da9-9ad8-4458-9654-90823be0d5f6&act=fp/serveapply"
 
-    login_Header = {
-        'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Mobile Safari/537.36',
-        'content-type': "application/x-www-form-urlencoded",
-    }
-    
-    login_Cookie = {
-        'JSESSIONID': secrets.token_hex(16).upper()
-    }
-
-    ul = str(len(username))
-    pl = str(len(password))
-
-    try:
-        session = requests.session()
-        r = session.get(login_url)
-
-        lt_execution = get_lt_And_execution(r)
-        lt = lt_execution['lt']
-        execution = lt_execution['execution']
-
-        rsa = generate_their_RSA(username=username, password=password, lt=lt)
-
-        login_Body = {
-            'rsa': rsa,
-            'ul': ul,
-            'pl': pl,
-            'lt': lt,
-            'execution': execution,
-            '_eventId': 'submit'
+        self.session = requests.session()
+        self.login_header = {
+            'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Mobile Safari/537.36',
+            'content-type': "application/x-www-form-urlencoded",
+        }
+        self.login_cookie = {
+            'JSESSIONID': secrets.token_hex(16).upper()
         }
 
-        result = session.post(login_url, headers=login_Header,
-                              cookies=login_Cookie, data=login_Body)
+    def health_login(self):
+        try:
+            ul = str(len(self.username))
+            pl = str(len(self.password))
+            r = self.session.get(self.login_url)
 
-        # result = session.get("http://service.sdu.edu.cn/tp_up/view?m=up")
+            lt_execution = get_lt_And_execution(r)
+            lt = lt_execution['lt']
+            execution = lt_execution['execution']
 
-        result = session.get(
-            "https://service.sdu.edu.cn/tp_up/view?m=up#act=portal/viewhome")
+            rsa = generate_their_RSA(
+                username=self.username, password=self.password, lt=lt)
 
-        print(result.text)
+            login_body = {
+                'rsa': rsa,
+                'ul': ul,
+                'pl': pl,
+                'lt': lt,
+                'execution': execution,
+                '_eventId': 'submit'
+            }
 
-        print('OK')
-    except:
-        print('?')
+            result = self.session.post(self.login_url, headers=self.login_header,
+                                       cookies=self.login_cookie, data=login_body)
+
+            # result = session.get("http://service.sdu.edu.cn/tp_up/view?m=up")
+
+            result = self.session.get(
+                "https://service.sdu.edu.cn/tp_up/view?m=up#act=portal/viewhome")
+
+            print(result)
+        except:
+            print('?')
 
 
 if __name__ == "__main__":
     user = ''
     password = ''
-    login(username=user,password=password)
+    sdu = SduHealth(username=user, password=password)
+    sdu.health_login()
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+sduHealth.health_login()
