b5adde16a9b9d20d08f34ef25e39f4a291bf851b
viewv <zxnnet@gmail.com>
2021-01-19 14:11:43 +0800

    Try to fix bugs

--- model.py
+++ model.py
@@ -6,8 +6,9 @@ import demjson as json
 TIME_ZONE = 'Asia/Shanghai'
 
 
-def test():
+def test(data):
     print("Model!!!!")
+    print(data)
 
 
 def get_current_date(timezone):
@@ -30,7 +31,7 @@ def generate_post_data(source_data):
     current_date_time = current_date + ' 07:00:00'
     current_timestamp = get_current_stamp()
 
-    source_record = source_data["535b1ef6-bf51-4d4c-9ae4-5a90cdc4_record"]["rowSet"]["primary"][0]
+    source_record = source_data["body"]["dataStores"]["535b1ef6-bf51-4d4c-9ae4-5a90cdc4_record"]["rowSet"]["primary"][0]
 
     zh = source_record["ZH"]                    # student id
     xm = source_record["XM"]                    # student name
@@ -56,7 +57,6 @@ def generate_post_data(source_data):
     dqjzdz = source_record["DQJZDZ"]            # location
     clsj = current_date_time                    # temp. time
 
-    source_vars = source_data["variable"]["rowSet"]["primary"]
 
     # SYS_USER = source_vars[0]["value"]          # student name
     # SYS_UNIT = source_vars[1]["value"]          # student unit
@@ -68,36 +68,36 @@ def generate_post_data(source_data):
     # ZYMC = source_vars[7]["value"]              # student major
     # MOBILE = source_vars[8]["value"]            # mobile
 
-    model_data["535b1ef6-bf51-4d4c-9ae4-5a90cdc4"]["rowSet"]["primary"][0]["ZH"] = zh
-    model_data["535b1ef6-bf51-4d4c-9ae4-5a90cdc4"]["rowSet"]["primary"][0]["XM"] = xm
-    model_data["535b1ef6-bf51-4d4c-9ae4-5a90cdc4"]["rowSet"]["primary"][0]["XSXB"] = xsxb
-    model_data["535b1ef6-bf51-4d4c-9ae4-5a90cdc4"]["rowSet"]["primary"][0]["NL"] = nl
-    model_data["535b1ef6-bf51-4d4c-9ae4-5a90cdc4"]["rowSet"]["primary"][0]["SZDW"] = szdw
-    model_data["535b1ef6-bf51-4d4c-9ae4-5a90cdc4"]["rowSet"]["primary"][0]["ZYMC"] = zymc
-    model_data["535b1ef6-bf51-4d4c-9ae4-5a90cdc4"]["rowSet"]["primary"][0]["XSLX"] = xslx
-    model_data["535b1ef6-bf51-4d4c-9ae4-5a90cdc4"]["rowSet"]["primary"][0]["ZXSJ"] = zxsj
-    model_data["535b1ef6-bf51-4d4c-9ae4-5a90cdc4"]["rowSet"]["primary"][0]["SBSJ"] = sbsj
-    model_data["535b1ef6-bf51-4d4c-9ae4-5a90cdc4"]["rowSet"]["primary"][0]["FDYXMX"] = fdyxmx
-    model_data["535b1ef6-bf51-4d4c-9ae4-5a90cdc4"]["rowSet"]["primary"][0]["JJLXRXM"] = jjlxrxm
-    model_data["535b1ef6-bf51-4d4c-9ae4-5a90cdc4"]["rowSet"]["primary"][0]["JJLXRDH"] = jjlxrdh
-    model_data["535b1ef6-bf51-4d4c-9ae4-5a90cdc4"]["rowSet"]["primary"][0]["JJLXRYBRGX"] = jjlxrybrgx
-    model_data["535b1ef6-bf51-4d4c-9ae4-5a90cdc4"]["rowSet"]["primary"][0]["LXZT"] = lxzt
-    model_data["535b1ef6-bf51-4d4c-9ae4-5a90cdc4"]["rowSet"]["primary"][0]["DQSFJJIA"] = dqsfjjia
-    model_data["535b1ef6-bf51-4d4c-9ae4-5a90cdc4"]["rowSet"]["primary"][0]["sheng_TEXT"] = sheng_text
-    model_data["535b1ef6-bf51-4d4c-9ae4-5a90cdc4"]["rowSet"]["primary"][0]["sheng"] = sheng
-    model_data["535b1ef6-bf51-4d4c-9ae4-5a90cdc4"]["rowSet"]["primary"][0]["shi_TEXT"] = shi_text
-    model_data["535b1ef6-bf51-4d4c-9ae4-5a90cdc4"]["rowSet"]["primary"][0]["shi"] = shi
-    model_data["535b1ef6-bf51-4d4c-9ae4-5a90cdc4"]["rowSet"]["primary"][0]["quxian_TEXT"] = quxian_text
-    model_data["535b1ef6-bf51-4d4c-9ae4-5a90cdc4"]["rowSet"]["primary"][0]["quxian"] = quxian
-    model_data["535b1ef6-bf51-4d4c-9ae4-5a90cdc4"]["rowSet"]["primary"][0]["DQJZDZ"] = dqjzdz
-    model_data["535b1ef6-bf51-4d4c-9ae4-5a90cdc4"]["rowSet"]["primary"][0]["CLSJ"] = clsj
+    model_data["body"]["dataStores"]["535b1ef6-bf51-4d4c-9ae4-5a90cdc4"]["rowSet"]["primary"][0]["ZH"] = zh
+    model_data["body"]["dataStores"]["535b1ef6-bf51-4d4c-9ae4-5a90cdc4"]["rowSet"]["primary"][0]["XM"] = xm
+    model_data["body"]["dataStores"]["535b1ef6-bf51-4d4c-9ae4-5a90cdc4"]["rowSet"]["primary"][0]["XSXB"] = xsxb
+    model_data["body"]["dataStores"]["535b1ef6-bf51-4d4c-9ae4-5a90cdc4"]["rowSet"]["primary"][0]["NL"] = nl
+    model_data["body"]["dataStores"]["535b1ef6-bf51-4d4c-9ae4-5a90cdc4"]["rowSet"]["primary"][0]["SZDW"] = szdw
+    model_data["body"]["dataStores"]["535b1ef6-bf51-4d4c-9ae4-5a90cdc4"]["rowSet"]["primary"][0]["ZYMC"] = zymc
+    model_data["body"]["dataStores"]["535b1ef6-bf51-4d4c-9ae4-5a90cdc4"]["rowSet"]["primary"][0]["XSLX"] = xslx
+    model_data["body"]["dataStores"]["535b1ef6-bf51-4d4c-9ae4-5a90cdc4"]["rowSet"]["primary"][0]["ZXSJ"] = zxsj
+    model_data["body"]["dataStores"]["535b1ef6-bf51-4d4c-9ae4-5a90cdc4"]["rowSet"]["primary"][0]["SBSJ"] = sbsj
+    model_data["body"]["dataStores"]["535b1ef6-bf51-4d4c-9ae4-5a90cdc4"]["rowSet"]["primary"][0]["FDYXMX"] = fdyxmx
+    model_data["body"]["dataStores"]["535b1ef6-bf51-4d4c-9ae4-5a90cdc4"]["rowSet"]["primary"][0]["JJLXRXM"] = jjlxrxm
+    model_data["body"]["dataStores"]["535b1ef6-bf51-4d4c-9ae4-5a90cdc4"]["rowSet"]["primary"][0]["JJLXRDH"] = jjlxrdh
+    model_data["body"]["dataStores"]["535b1ef6-bf51-4d4c-9ae4-5a90cdc4"]["rowSet"]["primary"][0]["JJLXRYBRGX"] = jjlxrybrgx
+    model_data["body"]["dataStores"]["535b1ef6-bf51-4d4c-9ae4-5a90cdc4"]["rowSet"]["primary"][0]["LXZT"] = lxzt
+    model_data["body"]["dataStores"]["535b1ef6-bf51-4d4c-9ae4-5a90cdc4"]["rowSet"]["primary"][0]["DQSFJJIA"] = dqsfjjia
+    model_data["body"]["dataStores"]["535b1ef6-bf51-4d4c-9ae4-5a90cdc4"]["rowSet"]["primary"][0]["sheng_TEXT"] = sheng_text
+    model_data["body"]["dataStores"]["535b1ef6-bf51-4d4c-9ae4-5a90cdc4"]["rowSet"]["primary"][0]["sheng"] = sheng
+    model_data["body"]["dataStores"]["535b1ef6-bf51-4d4c-9ae4-5a90cdc4"]["rowSet"]["primary"][0]["shi_TEXT"] = shi_text
+    model_data["body"]["dataStores"]["535b1ef6-bf51-4d4c-9ae4-5a90cdc4"]["rowSet"]["primary"][0]["shi"] = shi
+    model_data["body"]["dataStores"]["535b1ef6-bf51-4d4c-9ae4-5a90cdc4"]["rowSet"]["primary"][0]["quxian_TEXT"] = quxian_text
+    model_data["body"]["dataStores"]["535b1ef6-bf51-4d4c-9ae4-5a90cdc4"]["rowSet"]["primary"][0]["quxian"] = quxian
+    model_data["body"]["dataStores"]["535b1ef6-bf51-4d4c-9ae4-5a90cdc4"]["rowSet"]["primary"][0]["DQJZDZ"] = dqjzdz
+    model_data["body"]["dataStores"]["535b1ef6-bf51-4d4c-9ae4-5a90cdc4"]["rowSet"]["primary"][0]["CLSJ"] = clsj
 
-    model_data["535b1ef6-bf51-4d4c-9ae4-5a90cdc4_record"] = source_data["535b1ef6-bf51-4d4c-9ae4-5a90cdc4_record"]
-    model_data["535b1ef6-bf51-4d4c-9ae4-5a90cdc4_record"]["rowSet"]["primary"][0]["CLSJ"] = current_timestamp
-    model_data["535b1ef6-bf51-4d4c-9ae4-5a90cdc4_record"]["rowSet"]["primary"][0]["SBSJ"] = current_timestamp
+    model_data["body"]["dataStores"]["535b1ef6-bf51-4d4c-9ae4-5a90cdc4_record"] = source_data["body"]["dataStores"]["535b1ef6-bf51-4d4c-9ae4-5a90cdc4_record"]
+    model_data["body"]["dataStores"]["535b1ef6-bf51-4d4c-9ae4-5a90cdc4_record"]["rowSet"]["primary"][0]["CLSJ"] = current_timestamp
+    model_data["body"]["dataStores"]["535b1ef6-bf51-4d4c-9ae4-5a90cdc4_record"]["rowSet"]["primary"][0]["SBSJ"] = current_timestamp
 
-    model_data["variable"] = source_data["variable"]
-    model_data["parameters"] = source_data["parameters"]
+    model_data["body"]["dataStores"]["variable"] = source_data["body"]["dataStores"]["variable"]
+    model_data["body"]["parameters"] = source_data["body"]["parameters"]
 
     json.encode_to_file("./example.json", overwrite=True)
 
